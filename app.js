8e6499aa45fe00a863afaf9c08374255e8ee3588
rafaelg <desenvolvedor.rafael@gmal.com>
2019-12-18 19:01:32 -0400

    .

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 @version 1.0
 **/
 var app = require('./config/server.js');
+var porta = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     res.render('index');
@@ -13,6 +14,6 @@ app.get('/sistemas', function(req, res){
     res.sendFile(__dirname + '/app/views/sistemas.html');
 });
 
-app.listen(process.env.PORT,function(){
-    console.log('Servidor ON'+process.env.PORT)
+app.listen(porta,function(){
+    console.log('Servidor ON')
 });
